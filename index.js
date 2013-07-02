7932f758531b53eb52e95d3166b81b7910dfb758
Stuart McFarlane <stuart@mcfarlane.nl>
2013-07-02 11:13:25 +0200

    using  xmlshim
    The DOM construction works now but the docx conversion is less
    functional than I hoped it might be. Putting it blubtly it doesn't
    work at all.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 var stream = require('stream');
 var extend = require('extend');
-
 var Transform = require('stream').Transform;
+var jsdom = require('jsdom');
+var docx = require('./lib/docx.js');
 
-var toDocx = new Transform({decodeStrings: false});
+var toDocx = function constroctToDocx(options) {
+  var transform = new Transform({decodeStrings: false});
+  transform.options = options;
+  transform.htmlBuffer = null;
 
-extend(toDocx, {
-	htmlBuffer: null
-});
+  transform._transform = function _transform(chunk, encoding, done) {
+    console.log('transform');
+    if (!this.htmlBuffer) {
+      this.htmlBuffer = new Buffer(chunk.length);
+      chunk.copy(this.htmlBuffer);
+    } else {
+      this.htmlBuffer = Buffer.concat([this.htmlBuffer, chunk]);
+    }
+    done(null, null);
+  };
 
-toDocx._transform = function _transform(chunk, encoding, done) {
-  console.log('transform');
-  if (!this.htmlBuffer) {
-    this.htmlBuffer = new Buffer(chunk.length);
-    chunk.copy(this.htmlBuffer);
-  } else {
-    this.htmlBuffer = Buffer.concat([this.htmlBuffer, chunk]);
-  }
-  done(null, null);
-};
-
-toDocx._flush = function _flush(done) {
-  console.log('flush');
-  this.push(this.htmlBuffer);
-  done(null);
+  transform._flush = function _flush(done) {
+    console.log('flush');
+    var htmlAsString = this.htmlBuffer.toString();
+    var htmlAsDom = jsdom.jsdom({
+      url: options.url,
+      html: htmlAsString,
+    });
+    console.log('got dom');
+    var htmlAsDocx = docx.convertContent(htmlAsDom);
+    console.log('got docx', htmlAsDocx);
+    this.push(htmlAsDocx);
+    done(null);
+  };
+  return transform;
 };
 
 module.exports = {
